a81ca9ee8992dde447327479cbb5710df9bab9b3
Alam Hosen <alamhosen.web@gmail.com>
2022-03-26 11:57:42 +0600

    product added

---
+++ src/components/Product/Product.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css';
+
+const Product = (props) => {
+    const {name, price, img, ratings} = props.product;
+    return (
+        <div className='product'>
+             <img style={{width:'286px', margin:'7px'}} src={img} alt=""></img>
+            <div className='product-info'>
+             <h4 className='product-name'>{name}</h4>
+             <p>Price: ${price}</p>
+             <p><small>Ratings: {ratings} Stars</small></p>
+            </div>
+
+            <button className='btn-cart'>
+                <p className='btn-text'>Add to Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+            </button>
+            
+        </div>
+    );
+};
+
+export default Product;
